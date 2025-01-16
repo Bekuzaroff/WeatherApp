b6afa0260f6f23c3cddc3c3bc0451a40f817fda9
chermen <bekuzarovchermen@gmail.com>
2025-01-16 14:35:24 +0300

    generated weather models

---
+++ app/src/main/java/com/example/weatherapp/domain/models/Current.kt
@@ -0,0 +1,24 @@
+package com.example.weatherapp.domain.models
+
+data class Current(
+    val cloud: Int,
+    val condition: Condition,
+    val feelslike_c: Double,
+    val feelslike_f: Double,
+    val gust_kph: Double,
+    val gust_mph: Double,
+    val humidity: Int,
+    val is_day: Int,
+    val precip_in: Double,
+    val precip_mm: Double,
+    val pressure_in: Double,
+    val pressure_mb: Double,
+    val temp_c: Double,
+    val temp_f: Double,
+    val vis_km: Double,
+    val vis_miles: Double,
+    val wind_degree: Int,
+    val wind_dir: String,
+    val wind_kph: Double,
+    val wind_mph: Double
+)
--- app/src/main/java/com/example/weatherapp/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/activities/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.example.weatherapp
+package com.example.weatherapp.presentation.activities
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
