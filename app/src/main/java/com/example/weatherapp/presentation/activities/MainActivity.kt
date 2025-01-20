c69ef856f69d4af8a0145406939e445f6b8bc91d
chermen <bekuzarovchermen@gmail.com>
2025-01-20 16:00:17 +0300

    finished drawer ui

--- app/src/main/java/com/example/weatherapp/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/activities/MainActivity.kt
@@ -3,11 +3,24 @@ package com.example.weatherapp.presentation.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherapp.R
+import com.example.weatherapp.databinding.ActivityMainBinding
+import com.example.weatherapp.presentation.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // start fragment
+        supportFragmentManager.beginTransaction().replace(R.id.fragmenthost,
+            HomeFragment()).commit()
     }
+
+
 }
